f48c647dad4825ea52bae7ceb1bf82fc1af7f9c3
SimonGreenhill <SimonGreenhill@users.noreply.github.com>
2022-05-19 21:17:36 +1200

    import data

--- cldfbench_bouckaert_et_al2018.py
+++ cldfbench_bouckaert_et_al2018.py
@@ -9,20 +9,16 @@ class Dataset(phlorest.Dataset):
 
     def cmd_makecldf(self, args):
         self.init(args)
-        args.writer.add_summary(
-            self.raw_dir.read_tree('41559_2018_489_MOESM4_ESM.txt'),
-            self.metadata,
-            args.log)
-        posterior = self.sample(
-            self.remove_burnin(
-                self.raw_dir.read('pny10.fixed.cov.ucln.bdsky.ba-sp.trees.gz'),
-                1000),
-            detranslate=True,
-            as_nexus=True)
-        args.writer.add_posterior(
-            posterior.trees.trees,
-            self.metadata,
-            args.log)
+
+        summary = self.raw_dir.read_tree(
+            '41559_2018_489_MOESM4_ESM.txt', detranslate=False)
+        args.writer.add_summary(summary, self.metadata, args.log)
+
+        posterior = self.raw_dir.read_trees(
+           'pny10.fixed.cov.ucln.bdsky.ba-sp.trees.gz',
+           burnin=1000, sample=1000, detranslate=True)
+        args.writer.add_posterior(posterior, self.metadata, args.log)
+
         args.writer.add_data(
             phlorest.BeastFile(self.raw_dir / '41559_2018_489_MOESM3_ESM.xml'),
             self.characters,
